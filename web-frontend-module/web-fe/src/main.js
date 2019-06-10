3d6b63e8f9d95a89490924f282a0ee194e82967e
冯江龙 <jianglong.feng@ucarinc.com>
2019-06-10 17:11:47 +0800

    添加mock

--- web-frontend-module/web-fe/src/main.js
+++ web-frontend-module/web-fe/src/main.js
@@ -1,7 +1,10 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import 'normalize.css/normalize.css' // A modern alternative to CSS resets
 
+import 'element-ui/lib/theme-chalk/index.css'
+import '@/styles/index.scss' // global css
 import router from './router'
 import store from './store'
 import Element from 'element-ui'
@@ -9,9 +12,16 @@ import Cookies from 'js-cookie'
 import App from './App'
 import '@/icons' // icon 直接在自身的js中声明为Vue的components了
 import '@/permission' // permission control
+import { mockXHR } from '../mock'
 Vue.use(Element, {
   size: Cookies.get('size') || 'medium'
 })
+
+if (process.env.NODE_ENV === 'production') {
+  mockXHR()
+}
+// set Element lang to EN
+Vue.use(Element)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
