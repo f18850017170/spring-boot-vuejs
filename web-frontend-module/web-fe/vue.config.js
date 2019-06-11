59a59b29b4f5e86cd8979c8d56b1b58bff1ba372
冯江龙 <jianglong.feng@ucarinc.com>
2019-06-11 17:51:57 +0800

    VUE_CLI_BABEL_TRANSPILE_MODULES = true 核心配置，支持ES6语法

--- web-frontend-module/web-fe/vue.config.js
+++ web-frontend-module/web-fe/vue.config.js
@@ -1,3 +1,4 @@
+//  vue.config.js 是一个可选的配置文件，如果项目的 (和 package.json 同级的) 根目录中存在这个文件，那么它会被 @vue/cli-service 自动加载
 'use strict'
 const path = require('path')
 const defaultSettings = require('./src/settings.js')
